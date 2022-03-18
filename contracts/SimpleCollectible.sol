2c96a35a51714ce3dcc271c456836ab25dbf45a9
uzair011 <8407uzair@gmail.com>
2022-03-18 23:27:29 +0530

    second commit, before the advance NFT upgrades...

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
 
-    constructor() public ERC721("Dogie", "DOG") {
+    constructor() public ERC721("happyDog", "DOG") {
         tokenCounter = 0;
     }
 
--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,9 +1,22 @@
 from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectible, accounts, network, config
+
+sampleCollectibleURI = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
-from scripts.helpful_scripts import get_account, SimpleCollectible
+def deploy_and_create():
+    account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    tracsaction = simple_collectible.createCollectible(
+        sampleCollectibleURI, {"from": account}
+    )
+    tracsaction.wait(1)
+    print(
+        f"Congratulations, You can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}"
+    )
+    return simple_collectible
 
 
 def main():
-    account = get_account()
-    simple_collectible = SimpleCollectible.deploy({"form": account})
+    deploy_and_create()
